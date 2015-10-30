8fca15f3a2c34f25a44407a157265538e41aa4f3
Leslie Devlin <ldevlin@cloudpassage.com>
2015-10-30 16:13:08 -0700

    Pretty up the script; update README.md

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # retire_servers
+
+This Python script will retire any servers that have not been detected by Halo after several days.
+This will help large cloud environments with high server turnover reduce the clutter in their
+Halo accounts. 
+
+The script takes advantage of the new server field last_state_change and is intended to be run from
+cron on a tools server or bastion box once daily.  Since last_state_change is only expressed
+in whole days after a server has been offline for more than 24 hours, there is little advantage
+to running it more often.
+
+You will need a config.py file that includes your API key and secret key -- see the sample file.
+The keypair must have full access (read-write).  You will also need a version of cpapi from
+October 30, 2015 or later.
+
+By default, servers that have been deactivated for more than 7 days will be retired by the script.
+To change this, modify the value of retire_days.
+
+Note that servers must be deactivated before they can be moved into the retired group.
+Missing servers should be investigated rather than automatically retired.
+
--- retire_servers.py
+++ retire_servers.py
@@ -26,36 +26,37 @@ def get_server_data():
         server_id = server['id']
         server_hostname = server['hostname']
 
-        # create aware datetime object for last time seen
+        # How many days should a server be offline before being retired?
+        retire_days = 7
+
+        # Create aware datetime object for last time seen
 	lastseen = iso8601.parse_date(server['last_state_change'])
 
-        # create aware datetime object for current time
+        # Create aware datetime object for current time
         utc = pytz.timezone('UTC')
         utcnow = datetime.datetime.utcnow()
         utcnow_aware = utc.fromutc(utcnow)
-	print "%s (id %s) was last seen at %s." % (server_hostname, server_id, lastseen)
 
-        # calculate time diff in days
-        # after 1 day, last_state_change rounds off to days
+        # Calculate time diff in days
+        # After 1 day, last_state_change rounds off to days
         time_diff = utcnow_aware - lastseen
-	print "Last seen %s, now %s.\n" % (lastseen, utcnow_aware)
-	print "Server was last seen %s days ago.\n" % int(time_diff.days)
-        print "Hostname: %s, ID %s\n" % (server_hostname, server_id)
+        diff_days = int(time_diff.days)
 
-        # don't retire a server that's already retired
+        # Don't retire a server that's already retired
         if server['group_name'] == 'Retired':
-            print "Server %s already retired -- ignoring." % server_id
-        # if server older than 7 days, move to retired
-        elif (int(time_diff.days) > 7 and server_id):
+            print "Server %s already retired -- ignoring." % server_hostname
+        # If server older than retire_days days, move to retired
+        elif (diff_days > retire_days and server_id):
             data, autherr = apiCon.doRetireServer(server_id)
             if not autherr:
-                print "Server %s retired successfully." % server_id
+                print "Server %s retired successfully." % server_hostname
         else:
             print "Unable to move server."
             if not server_id:
-                print "Server: %s does not exist.\n" % server_id
-            elif int(time_diff.days) <= 7:
-                print "Server %s has been offline for fewer than 7 days.\n" % server_id
+                print "Server: %s (id %s) does not exist.\n" % (server_hostname, server_id)
+            elif diff_days <= retire_days:
+                print "Server %s has been offline for %s days.\n" % (server_hostname, diff_days)
+
 
 
 #---MAIN---------------------------------------------------------------------
